925f6c9d8a318749aa44fac708e96aa25eaa61fe
nishanmohammed <nishanmohammed@nishans-air>
2024-03-17 23:21:52 +0400

    week 7-8 commit

--- src/hangman/Run.java
+++ src/hangman/Run.java
@@ -1,8 +1,16 @@
 package hangman;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Run {
 
 	public static void main(String[] args) {
+		
+
+			MainWindow mainWindow = new MainWindow();
+			
+		
 		
 	}
 
